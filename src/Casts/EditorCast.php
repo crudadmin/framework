9f93888f4965746026f3e293d7769d73571578bb
Marek Gogol <info@marekgogol.sk>
2025-10-17 11:23:06 +0200

    fix

--- src/Casts/EditorCast.php
+++ src/Casts/EditorCast.php
@@ -27,7 +27,7 @@ class EditorCast implements CastsAttributes, AdminCast
                 $attributes = 'data-model="'.$model->getTable().'" data-key="'.$key.'" data-id="'.$model->getKey().'" data-hash="'.$hash.'"';
             }
 
-            return '<div data-crudadmin-editor'.$attributes.'>'.$value.'</div>';
+            return $value ? '<div data-crudadmin-editor'.$attributes.'>'.$value.'</div>' : null;
         }
 
         return $value;
